10c48e0cf3181f8b1f7147ac94ba175b17791f0e
nfd9001 <fakereplacelater@example.com>
2016-12-16 02:22:11 -0800

    Clean up some messes

--- app/src/main/java/com/nfd/libgenscan/BookRef.java
+++ app/src/main/java/com/nfd/libgenscan/BookRef.java
@@ -1,20 +1,21 @@
 package com.nfd.libgenscan;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import me.dm7.barcodescanner.zbar.BarcodeFormat;
 import android.content.Intent;
 import android.net.Uri;
-import android.app.Activity;
+
 /**
  * @author Alexander Ronsse-Tucherov
  * @version 2016-08-26.
- *
- * Simple String wrapper to hold a reference to some book; provided for options for future expansion
- * (e.g. UPC manipulation)
+ *          Wrapper to hold a reference to some book; provided for options for future expansion (e.g. UPC manipulation)
  */
 class BookRef {
-    private static ArrayList<BookRef> list = new ArrayList<>();
-    private ArrayList<BarcodeFormat> allowedFormats = new ArrayList<BarcodeFormat>();
+    private static ArrayList<BookRef> bookList = new ArrayList<>(); //currently dead, but likely will help with history
+    private List<BarcodeFormat> allowedFormats = Arrays.asList(BarcodeFormat.ISBN10, BarcodeFormat.ISBN13);
 
     private String id;
     private BarcodeFormat format;
@@ -24,10 +25,8 @@ class BookRef {
 
     public BookRef(String id, BarcodeFormat format, boolean opened, MainActivity parent) throws IllegalArgumentException {
 
-        allowedFormats.add(BarcodeFormat.ISBN10);
-        allowedFormats.add(BarcodeFormat.ISBN13);
 
-        if (!this.allowedFormats.contains(format)){
+        if (!isAllowed(format)) {
             throw new IllegalArgumentException("Format not supported");
         }
         this.id = id;
@@ -36,28 +35,35 @@ class BookRef {
         this.parent = parent;
     }
 
-    public String getId(){return id;}
-    public boolean isOpened(){return opened;}
-    public BarcodeFormat getFormat(){return format;}
-
-    static void addToList(BookRef b){
-        list.add(b);
+    public boolean isAllowed(BarcodeFormat b) {
+        return this.allowedFormats.contains(b);
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public boolean isOpened() {
+        return opened;
+    }
+
+    public BarcodeFormat getFormat() {
+        return format;
+    }
+
+    static void addToList(BookRef b) {
+        bookList.add(b);
+    }
+
+    //TODO: add more libraries, possibly ways of handling other types of barcodes (search by UPC?)
     public void searchBook() {
         opened = true;
-        //fire search intent
         String uri = "http://libgen.io/search.php?req=" + id +
                 "&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=identifier";
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-        //...how. How do I fire this; can't sent it to MainIntent easily because this is static
-        //how bout this?
         parent.fire(browserIntent);
 
     }
-
-
-
 
 
 }
--- app/src/main/java/com/nfd/libgenscan/MainActivity.java
+++ app/src/main/java/com/nfd/libgenscan/MainActivity.java
@@ -1,18 +1,18 @@
 package com.nfd.libgenscan;
 
-import android.net.Uri;
 import android.os.Build;
 import android.widget.Toast;
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import me.dm7.barcodescanner.core.BarcodeScannerView;
 import me.dm7.barcodescanner.zbar.*;
 import android.content.Intent;
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 
+
+/* Scanning UI and main menu. This should always be the first thing the user sees on launch.
+ * TODO: add history menu, autoscan option, and restore support for pre-Marshmallow if possible
+ */
 public class MainActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler {
     private ZBarScannerView mScannerView;
     private boolean autosearch = true; //TODO: add switching (new Activity, along with menu of bookrefs)
@@ -26,39 +26,29 @@ public class MainActivity extends AppCompatActivity implements ZBarScannerView.R
         setContentView(mScannerView);                // Set the scanner view as the content view
 
 
-
-
-
-
-        // Request permission. This does it asynchronously so we have to wait for onRequestPermissionResult before trying to open the camera.
+        // Request permission. This does it asynchronously so we have to wait for onRequestPermissionResult before
+        // trying to open the camera.
         if (!haveCameraPermission())
             requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
     }
 
-    private boolean haveCameraPermission()
-    {
+    private boolean haveCameraPermission() {
         if (Build.VERSION.SDK_INT < 23)
             return true;
         return checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
-    {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         // This is because the dialog was cancelled when we recreated the activity.
         if (permissions.length == 0 || grantResults.length == 0)
             return;
 
-        switch (requestCode)
-        {
-            case PERMISSION_REQUEST_CAMERA:
-            {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     mScannerView.startCamera();
-                }
-                else
-                {
+                } else {
                     finish();
                 }
             }
@@ -81,22 +71,19 @@ public class MainActivity extends AppCompatActivity implements ZBarScannerView.R
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
         mScannerView.stopCamera();
         mScannerView = null;
     }
 
-    void fire(Intent i){
+    //intended to fire URL opener intents from searches
+    void fire(Intent i) {
         startActivity(i);
     }
 
     @Override
     public void handleResult(Result rawResult) {
-
-        //TODO: Switch to using BookRef handling; later, add abstract handler implementations
-        // per-format? Or maybe don't bother, this is small, and I expect like two two different blobs of code tops
-        String format = rawResult.getBarcodeFormat().getName();
 
         try {
 
@@ -105,7 +92,7 @@ public class MainActivity extends AppCompatActivity implements ZBarScannerView.R
 
             //remove; if set to auto-open, immediately call openers before throwing ref out
             // gotta figure out settings activities first
-            if (autosearch){
+            if (autosearch) {
                 b.searchBook();
             }
 
@@ -118,23 +105,6 @@ public class MainActivity extends AppCompatActivity implements ZBarScannerView.R
         }
 
 
-
-        /*
-        if(format.equals("ISBN10") || format.equals("ISBN13")){
-            //send intent to browser
-            String uri = "http://libgen.io/search.php?req=" + rawResult.getContents() +
-                    "&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=identifier";
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-            startActivity(browserIntent);
-
-
-        } else {
-            //try again
-            Toast.makeText(getApplicationContext(),
-                    "Barcode does not appear to be a valid ISBN, try again", Toast.LENGTH_SHORT).show();
-            mScannerView.resumeCameraPreview(this);
-        }
-         */
     }
 
 }
